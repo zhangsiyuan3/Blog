eb0176415c63870ea8f1e55534ce8ce1ccf870c3
ShallowLan <15669147150@163.com>
2020-08-17 11:47:14 +0800

    解决问题

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -1,8 +1,4 @@
 import React,{Component} from 'react';
-<<<<<<< HEAD
-=======
-import "./index.scss"
->>>>>>> 59cdf84cbf312e23a6967717a7e0b021e7a38e14
 class Index extends Component {
     constructor(props) {
         super(props)
@@ -12,12 +8,13 @@ class Index extends Component {
         return (
             <div className="header">
                 <p className='title'>首页</p>
-                <p><i className='iconfont iconicon_search'></i></p>
-                <p><i className='iconfont iconicon_search'></i></p>
-                <p><i className='iconfont iconicon_search'></i></p>
-                <p><i className='iconfont iconicon_search'></i></p>
-                <p><i className='iconfont iconicon_search'></i></p>
-                <p><i className='iconfont iconicon_search'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
+                <p><i className='iconfont icon-soushuo'></i></p>
             </div>
         )
     }
